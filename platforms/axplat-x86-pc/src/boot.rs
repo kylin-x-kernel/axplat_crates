4ce333448554bb36326e47ed6b6e5ea602541c08
luozhixian <luozhixian@kylinos.cn>
2026-01-22 13:45:25 +0800

    sev feature
    Signed-off-by: luozhixian <luozhixian@kylinos.cn>

--- platforms/axplat-x86-pc/src/boot.rs
+++ platforms/axplat-x86-pc/src/boot.rs
@@ -31,7 +31,7 @@ const CR4: u64 = Cr4Flags::PHYSICAL_ADDRESS_EXTENSION.bits()
         0
     };
 const EFER: u64 = EferFlags::LONG_MODE_ENABLE.bits() | EferFlags::NO_EXECUTE_ENABLE.bits();
-const SEV_CBIT: u64 = 1u64 << 47;
+const SEV_CBIT: u64 = if cfg!(feature = "sev") { 1u64 << 47 } else { 0 };
 
 #[unsafe(link_section = ".bss.stack")]
 static mut BOOT_STACK: [u8; BOOT_STACK_SIZE] = [0; BOOT_STACK_SIZE];
